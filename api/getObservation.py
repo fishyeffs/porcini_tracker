19579dee8e6dd4b00221ae0863e57f42d46c8740
Nick Fisher <nickfish0311@gmail.com>
2025-07-14 23:15:52 +0100

    map functionality + slider

--- api/getObservation.py
+++ api/getObservation.py
@@ -1,20 +1,42 @@
-import requests, json
+import requests, math
 
 def getObservation():
+	per_page = 200
+	page=1
 	params = {
 		"taxon_id" : 48701,
 		"iconic_taxa" : "Fungi",
-		"per_page" : 200,
+		"per_page" : per_page,
 		"place_id" : 6857
 	}
 
-	response = requests.get("https://inaturalist.org./observations.json", params)
+	#to ensure we don't suckerpunch the api
+	desiredResult=1000
+	response = requests.get("https://inaturalist.org/observations.json", params)
 	mushroomList = response.json()
-	print("HTTP Response: ", response.status_code)
-
+	returnedResults=int(response.headers.get("X-Total-Entries",0))
+	#print(f"Number of results:  {returnedResults}")
+	## return pages per 1000, or if returnedResults is less than 1000, do not exceed returned pages
+	if (returnedResults > desiredResult):
+		pages = math.floor(desiredResult / per_page) 
+	else:
+		pages = math.floor(returnedResults / per_page)
+	#print("HTTP Response: ", response.json())
+	
+	while(page<pages):
+		page+=1
+		params = {
+			"taxon_id" : 48701,
+			"iconic_taxa" : "Fungi",
+			"per_page" : per_page,
+			"page" : page,
+			"place_id" : 6857
+		}
+		response = requests.get("https://inaturalist.org/observations.json", params)
+		mushroomList += response.json()
+	
 	for i in mushroomList:
 		i["latitude"] = float(i.get("latitude", 0))
 		i["longitude"] = float(i.get("longitude", 0))
-	#print(json.dumps(mushroomList, indent=2))
-
+	
 	return mushroomList
--- main.py
+++ main.py
@@ -3,45 +3,64 @@ from objects.mushroom import Mushroom, MushroomList
 from dash import Dash, html, dash_table, callback, Output, Input , dcc
 import plotly.express as px
 import pandas as pd
+import numpy as np
 
 def parseMushroomList():
 	mushroomList = getObservation()
 	porcinis = MushroomList(mushroomList)
 	return porcinis
 	
-porcinis = parseMushroomList()
-### example usage
-#for i in porcinis.getAll():
-#	print(f'{i.place} on {i.creation_time}, {i.month} | LAT: {i.latitude} LONG: {i.longitude}')
-	
-
 app = Dash()
 porcinis = parseMushroomList()
+#convert porcinis from mushroom objects to dictionary entries
 mushroomData = [mush.to_dict() for mush in porcinis.getAll()]
 
 df = pd.DataFrame(mushroomData)
-print(porcinis.getSize())
+months = np.arange(1,12)
+centre = {
+	"lat": df["latitude"].mean(),
+	"lon": df["longitude"].mean()
+}
+#print(porcinis.getSize())
 
-mapFig = px.scatter_map(
-    df,
-    lat="latitude",
-    lon="longitude",
-    hover_name="place",
-    hover_data=["month"],
-    color_discrete_sequence=["green"],
-    zoom=5,
-    height=600
-)
-
-mapFig.update_layout(mapbox_style="open-street-map")
-mapFig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-
+## dash app layout
 app.layout = [
-    html.H2(children="Porcini Geo"),
-    #dash_table.DataTable(data=mushroomData, page_size=10),
-    dcc.Graph(figure=mapFig),
-    dcc.Slider(id='monthSlider',min=1,max=1,step=1,value=6,marks={i: str(i) for i in range(1,13)},tooltip={"placement":"bottom","always_visible":True})
+	html.H2(children="Porcini Geo"),
+	html.Div(
+		dcc.Graph(id='mapFig'),
+		style={"width":"80%", "margin":"auto"}
+	),
+	html.Div(
+		dcc.Slider(id='monthSlider',min=1,max=12,step=1,value=6,marks={i: str(i) for i in range(1,13)},tooltip={"placement":"bottom","always_visible":True}),
+    	style={"width":"80%", "margin":"auto"}
+	),
+	dash_table.DataTable(data=mushroomData, page_size=10)
 ]
+
+## TODO could be worth putting all but filtering logic in a function in a separate file
+## app outputs : mapFig, return type scatter map
+## app inputs : monthSlider, type (dash) dccSlider
+@app.callback(
+      Output("mapFig", "figure"),
+      Input("monthSlider","value")
+)
+def update_map(curMonth):
+	monthDf = df[df["month"] == curMonth]
+	mapFig = px.scatter_map(
+		monthDf,
+		lat="latitude",
+		lon="longitude",
+		hover_name="place",
+		hover_data=["month"],
+		center=centre,
+		color_discrete_sequence=["green"],
+		zoom=5,
+		height=600
+	)
+
+	mapFig.update_layout(mapbox_style="open-street-map")
+	mapFig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+	return mapFig
 
 if __name__ == "__main__":
     app.run(debug=True)
